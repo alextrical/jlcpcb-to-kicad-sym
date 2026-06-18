08b78984691ecddcf59f39e280b5b79c2f297c42
alextrical <35117191+alextrical@users.noreply.github.com>
2026-06-18 15:26:04 +0100

    Refactor code to only use pulled symbols Add logic and lookup for non passive/3+ pin components Add component attributes to symbol

--- src/jlc_kicad_lib/generator.py
+++ src/jlc_kicad_lib/generator.py
@@ -1,23 +1,187 @@
 import re
-import sqlite3
+import sexpdata
 import sys
+import shutil
+import json
+
+from pathlib import Path
+from fnmatch import fnmatchcase
 
 sys.path.append("src/kicad-library-utils/common")
 import kicad_sym
-
 from kicad_sym import KicadLibrary, KicadSymbol, Property
-from patterns import PATTERNS
-from pathlib import Path
 
-def create_connection(db_path: str):
-    return sqlite3.connect(db_path)
+from library_definition import libraries, LibrarySpec
 
-OUTPUT_DIR = Path("build")
+def build_library(conn, spec: LibrarySpec, root_dir, output_folder) -> None:
+    output_dir = Path(root_dir) / Path(output_folder) / f"{spec.libname}.kicad_symdir"
+    output_dir.mkdir(parents=True, exist_ok=True)
 
-def create_library(libname: str, output_dir: Path = OUTPUT_DIR):
+    kwargs = dict(
+        conn=conn,
+        name_template=spec.name_template,
+        value_template=spec.value_template,
+        libname=spec.libname,
+        output_dir=output_dir,
+        root_dir=root_dir,
+        package_source=spec.package_source,
+        category_filter=spec.category_filter,
+    )
+
+    if spec.extends_symbol_lookup is not None:
+        kwargs["extends_symbol_lookup"] = spec.extends_symbol_lookup
+
+    if spec.extends_symbol is not None:
+        kwargs["extends_symbol"] = spec.extends_symbol
+
+    append_parts(**kwargs)
+
+def create_library(libname: str, output_dir: str):
     output_dir.mkdir(parents=True, exist_ok=True)
     library_path = output_dir / f"{libname}.kicad_sym"
     return KicadLibrary(str(library_path))
+
+def append_parts(conn, name_template, libname, output_dir, root_dir, category_filter, value_template, package_source,
+                 extends_symbol=None, extends_symbol_lookup=None, footprint=None,
+                 reference=None, ref_text_posx=None, ref_text_posy=None,
+                 val_text_posx=None, val_text_posy=None, ref_text_rotation=None,
+                 val_text_rotation=None, 
+                 ref_text_h_justify=None,ref_text_v_justify=None,
+                 val_text_h_justify=None,val_text_v_justify=None,
+                 hide_pin_numbers=None,hide_pin_names=None,
+                 pin_names_offset=None, keywords="", fp_filters="",
+                # value_template=None, symbol_pins=None, symbol_rectangles=None, symbol_polylines=None,
+                # symbol_arcs=None,  extends_symbol_name=None, extends_symbol_lookup=None,package_source=None
+                ):
+
+    for package_name, footprint_name, package_match in package_source():
+        where_clause = (
+            f"(library_type = 'base' OR preferred = 1) "
+            f"and {category_filter} "
+            f"and Package {package_match}"
+        )
+
+        cursor = conn.cursor()
+        cursor.execute(build_query(where_clause))
+
+        for lcsc_part, mfg_name, mfg_part, description, attributes, datasheet, stock, price in cursor.fetchall():
+            data = json.loads(attributes)
+            try:
+                value = eval(value_template)
+                if value is None:
+                    print(f"Skipping {lcsc_part}: no parsed value from {eval(value_template)!r}")
+                    continue
+            except Exception:
+                print(f"Can't parse value, skipping {lcsc_part} {eval(value_template)!r}")
+                continue
+
+            try:
+                name = eval(name_template)
+                if value is None:
+                    print(f"Skipping {lcsc_part}: no parsed name_template value from {eval(name_template)!r}")
+                    continue
+            except Exception:
+                print(f"Can't parse name_template, skipping {lcsc_part} {eval(name_template)!r}")
+                continue
+
+            extends_symbol_name = None
+            source_relpath = None
+            if extends_symbol_lookup is not None:
+                # source_relpath = extends_symbol_lookup.get(mfg_part)
+                for pattern, symbol in extends_symbol_lookup.items():
+                    if fnmatchcase(mfg_part, pattern):
+                        source_relpath = symbol
+                if source_relpath is  None:
+                    print(f"Skipping {lcsc_part}: no symbol match for {mfg_part}")
+                    continue
+            elif extends_symbol is not None:
+                source_relpath = extends_symbol
+            else:
+                print(f"Skipping {lcsc_part}: no symbol")
+                continue
+
+            if source_relpath is not None:
+                source_file = root_dir / Path("src/kicad-symbols") / source_relpath
+                shutil.copy(source_file, output_dir)
+
+                result = extract_symbol_properties(source_file)
+                extends_symbol_name = result.get("symbol_name")
+
+                properties = result.get("properties", {})
+
+                reference_prop = properties.get("Reference", {})
+                reference = reference_prop["value"]
+                ref_text_posx = reference_prop["x"]
+                ref_text_posy = reference_prop["y"]
+                ref_text_rotation = reference_prop["angle"]
+                ref_text_h_justify = reference_prop["justify"]
+
+                value_prop = properties.get("Value", {})
+                val_text_posx = value_prop["x"]
+                val_text_posy = value_prop["y"]
+                val_text_rotation = value_prop["angle"]
+                val_text_h_justify = value_prop["justify"]
+
+                fp_filters = properties.get("ki_fp_filters", {}).get("value")
+                keywords = properties.get("ki_keywords", {}).get("value")
+            
+            # print(keywords)
+
+            clean_name = re.sub(r'[\\/:*?"<>|]+', '_', name).strip(" .")
+            lib = create_library(clean_name,output_dir)
+
+            clean_description = re.sub(r'[^-A-Za-z 0-9%()℃~+-,±@Ω/\\.]', '', description.strip())
+            new_symbol = KicadSymbol.new(
+                name=name,
+                libname=libname,
+                datasheet=datasheet,
+                description=clean_description,
+                footprint=footprint_name,
+            )
+            lib.symbols.append(new_symbol)
+
+            ref = new_symbol.get_property("Reference")
+            apply_updates(ref, {
+                "value": reference,
+                "posx": ref_text_posx,
+                "posy": ref_text_posy,
+                "rotation": ref_text_rotation,
+                "effects.h_justify": ref_text_h_justify,
+                "effects.v_justify": ref_text_v_justify,
+            })
+
+            val = new_symbol.get_property("Value")
+            apply_updates(val, {
+                "value": value,
+                "posx": val_text_posx,
+                "posy": val_text_posy,
+                "rotation": val_text_rotation,
+                "effects.h_justify": val_text_h_justify,
+                "effects.v_justify": val_text_v_justify,
+            })
+
+            apply_updates(new_symbol, {
+                "extends": extends_symbol_name,
+                "hide_pin_numbers": hide_pin_numbers,
+                "pin_names_offset": pin_names_offset,
+                "hide_pin_names": hide_pin_names,
+                "keywords": keywords,
+                "fp_filters": fp_filters,
+            })
+
+            new_symbol.properties.append(Property("LCSC", lcsc_part, is_hidden=True))
+            new_symbol.properties.append(Property("MFG", mfg_name, is_hidden=True))
+            new_symbol.properties.append(Property("MFGPN", mfg_part, is_hidden=True))
+            new_symbol.properties.append(Property("Stock", str(stock), is_hidden=True))
+            new_symbol.properties.append(Property("unit_price_10", str(get_price(price, 10)), is_hidden=True))
+            new_symbol.properties.append(Property("unit_price_100", str(get_price(price, 100)), is_hidden=True))
+            new_symbol.properties.append(Property("unit_price_1000", str(get_price(price, 1000)), is_hidden=True))
+
+            # add values from attributes dynamically
+            for key, value in data.items():
+                new_symbol.properties.append(Property(key, value, is_hidden=True))
+
+            lib.write()
 
 def build_query(where_clause: str) -> str:
     return f"""
@@ -44,12 +208,30 @@ SELECT
             CROSS JOIN (SELECT Description AS desc_col) AS t
         ) AS description_replacements
     ) AS description,
+    attributes,
     datasheet,
     stock,
     price
 FROM jlc_components
 WHERE {where_clause}
 """
+
+
+#Written by AI
+def set_if_not_none(obj, attr_path, value):
+    if value is None:
+        return
+
+    target = obj
+    parts = attr_path.split(".")
+    for part in parts[:-1]:
+        target = getattr(target, part)
+    setattr(target, parts[-1], value)
+
+def apply_updates(obj, updates):
+    for attr_path, value in updates.items():
+        if value is not None:
+            set_if_not_none(obj, attr_path, value)
 
 def get_price(pricing_string, quantity):
     tiers = pricing_string.split(",")
@@ -63,111 +245,78 @@ def get_price(pricing_string, quantity):
         end = float("inf") if end == "" else int(end)
 
         if start <= quantity <= end:
-            return pricing
+            return f"${pricing}"
 
     raise ValueError(f"No pricing tier found for quantity {quantity}")
 
-def parse_matches(description: str, PATTERNS: dict):
-    matches = {}
-    for key, pattern in PATTERNS.items():
-        match = pattern.search(description)
-        matches[key] = match.group() if match else None
-    return matches
+def get_value_from_symbol(sexp):
+    """Extract value from sexpdata types"""
+    if isinstance(sexp, sexpdata.Symbol):
+        return sexp.value()
+    elif isinstance(sexp, sexpdata.String):
+        return sexp.value()
+    elif isinstance(sexp, str):
+        return sexp
+    elif isinstance(sexp, int) or isinstance(sexp, float):
+        return sexp
+    return sexp
 
+def extract_symbol_properties(file_path):
+    """Extract symbol name and properties from a KiCad .kicad_sym file"""
 
-def append_parts(conn, name_template, footprint, libname,
-                 where_clause, symbol_pins, output_dir, reference=None, ref_text_posx=None, ref_text_posy=None,
-                 val_text_posx=None, val_text_posy=None, ref_text_rotation=None,
-                 val_text_rotation=None, value_template=None,
-                 symbol_rectangles=None, symbol_polylines=None,
-                 symbol_arcs=None, keywords="", fp_filters="",
-                 ref_text_h_justify=None,ref_text_v_justify=None,
-                 val_text_h_justify=None,val_text_v_justify=None,
-                 hide_pin_numbers=None,hide_pin_names=None,
-                 pin_names_offset=None, extends_symbol_name=None):
-    cursor = conn.cursor()
-    cursor.execute(build_query(where_clause))
+    with open(file_path, "r") as f:
+        data = sexpdata.load(f)
 
-    for lcsc_part, mfg_name, mfg_part, description, datasheet, stock, price in cursor.fetchall():
-        try:
-            matches = parse_matches(description, PATTERNS)
-            value = eval(value_template)
-            name = eval(name_template)
-            if value is None:
-                print(f"Skipping {lcsc_part}: no parsed value from {description!r}")
-                continue
-        except Exception:
-            print(f"Can't parse, skipping {lcsc_part} {description!r}")
-            continue
+    result = {
+        "symbol_name": None,
+        "properties": {},
+    }
 
-        safe_name = re.sub(r'[\\/:*?"<>|]+', '_', name).strip(" .")
-        lib = create_library(safe_name,output_dir)
+    # Find top-level symbol
+    for item in data:
+        if isinstance(item, list) and len(item) > 1:
+            first = item[0]
+            if isinstance(first, sexpdata.Symbol) and first.value() == "symbol":
+                result["symbol_name"] = get_value_from_symbol(item[1])
 
-        clean_description = re.sub(r'[^-A-Za-z 0-9%()℃~+-,±@Ω/\\.]', '', description.strip())
-        new_symbol = KicadSymbol.new(
-            name=name,
-            libname=libname,
-            footprint=footprint,
-            datasheet=datasheet,
-            keywords=keywords,
-            description=clean_description,
-            fp_filters=fp_filters,
-        )
-        lib.symbols.append(new_symbol)
-        new_symbol.add_default_properties()
+                for part in item[2:]:
+                    if isinstance(part, list) and len(part) >= 3:
+                        part_first = part[0]
+                        if isinstance(part_first, sexpdata.Symbol) and part_first.value() == "property":
+                            prop_name = get_value_from_symbol(part[1])
+                            prop_value = get_value_from_symbol(part[2])
 
-        if extends_symbol_name is not None:
-            new_symbol.extends = extends_symbol_name
+                            x, y, angle = None, None, None
+                            justify = []
 
-        ref = new_symbol.get_property("Reference")
-        if reference is not None:
-            ref.value = reference
-        if ref_text_posx is not None:
-            ref.posx = ref_text_posx
-        if ref_text_posy is not None:
-            ref.posy = ref_text_posy
-        if ref_text_rotation is not None:
-            ref.rotation = ref_text_rotation
-        if ref_text_h_justify is not None:
-            ref.effects.h_justify = ref_text_h_justify
-        if ref_text_v_justify is not None:
-            ref.effects.v_justify = ref_text_v_justify
+                            for sub in part[3:]:
+                                if isinstance(sub, list) and len(sub) >= 1:
+                                    sub_first = sub[0]
 
-        val = new_symbol.get_property("Value")
-        val.value = value
-        if val_text_posx is not None:
-            val.posx = val_text_posx
-        if val_text_posy is not None:
-            val.posy = val_text_posy
-        if val_text_rotation is not None:
-            val.rotation = val_text_rotation
-        if val_text_h_justify is not None:
-            val.effects.h_justify = val_text_h_justify
-        if val_text_v_justify is not None:
-            val.effects.v_justify = val_text_v_justify
+                                    if isinstance(sub_first, sexpdata.Symbol):
+                                        if sub_first.value() == "at" and len(sub) >= 4:
+                                            x = get_value_from_symbol(sub[1])
+                                            y = get_value_from_symbol(sub[2])
+                                            angle = get_value_from_symbol(sub[3])
 
-        new_symbol.properties.append(Property("LCSC", lcsc_part, is_hidden=True))
-        new_symbol.properties.append(Property("MFG", mfg_name, is_hidden=True))
-        new_symbol.properties.append(Property("MFGPN", mfg_part, is_hidden=True))
-        new_symbol.properties.append(Property("Stock", str(stock), is_hidden=True))
-        new_symbol.properties.append(Property("Price@10", str(get_price(price, 10)), is_hidden=True))
-        new_symbol.properties.append(Property("Price@100", str(get_price(price, 100)), is_hidden=True))
-        new_symbol.properties.append(Property("Price@1000", str(get_price(price, 1000)), is_hidden=True))
+                                        elif sub_first.value() == "effects":
+                                            for effect_item in sub[1:]:
+                                                if isinstance(effect_item, list) and len(effect_item) >= 1:
+                                                    effect_first = effect_item[0]
+                                                    if (
+                                                        isinstance(effect_first, sexpdata.Symbol)
+                                                        and effect_first.value() == "justify"
+                                                    ):
+                                                        justify = [get_value_from_symbol(x) for x in effect_item[1:]]
 
-        if hide_pin_numbers is not None:
-            new_symbol.hide_pin_numbers = hide_pin_numbers
-        if pin_names_offset is not None:
-            new_symbol.pin_names_offset = pin_names_offset
-        if hide_pin_names is not None:
-            new_symbol.hide_pin_names = hide_pin_names
+                            result["properties"][prop_name] = {
+                                "value": prop_value,
+                                "x": x,
+                                "y": y,
+                                "angle": angle,
+                                "justify": justify,
+                            }
 
-        for pin in symbol_pins or []:
-            new_symbol.pins.append(pin)
-        for rectangle in symbol_rectangles or []:
-            new_symbol.rectangles.append(rectangle)
-        for polyline in symbol_polylines or []:
-            new_symbol.polylines.append(polyline)
-        for arc in symbol_arcs or []:
-            new_symbol.arcs.append(arc)
+                break
 
-        lib.write()
+    return result
---
+++ src/jlc_kicad_lib/library_definition.py
@@ -0,0 +1,109 @@
+from dataclasses import dataclass
+from typing import Callable, Optional
+
+from footprint_lookup import *
+
+@dataclass(frozen=True)
+class LibrarySpec:
+    libname: str
+    package_source: Callable
+    name_template: str
+    value_template: str
+    category_filter: str
+    extends_symbol: Optional[str] = None
+    extends_symbol_lookup: Optional[dict[str, str]] = None
+    reference: Optional[str] = None
+
+libraries = [
+    LibrarySpec(
+        libname="JLCPCB_Basic_Diode-General",
+        extends_symbol="Device.kicad_symdir/D.kicad_sym",
+        package_source=diode_packages,
+        name_template="mfg_part",
+        value_template="mfg_part",
+        category_filter='category="Diodes" and "Subcategory"="Diodes - General Purpose"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Resistor",
+        extends_symbol="Device.kicad_symdir/R.kicad_sym",
+        package_source=resistor_packages,
+        name_template = '"_".join(filter(None,[data.get("Resistance"),package_name,data.get("Tolerance", "")]))',
+        value_template="re.sub(r'[^a-zA-Z0-9.]', '', data['Resistance'])",
+        category_filter='"Category" = "Resistors" and "Subcategory" = "Chip Resistor - Surface Mount"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Capacitor_MLCC",
+        extends_symbol="Device.kicad_symdir/C.kicad_sym",
+        package_source=capacitor_packages,
+        name_template = '"_".join(filter(None,[data.get("Capacitance"),package_name,data.get("Voltage Rating", ""),data.get("Temperature Coefficient", ""),data.get("Tolerance", "")]))',
+        value_template="data['Capacitance']",
+        category_filter='"Category" = "Capacitors" and "Subcategory" = "Multilayer Ceramic Capacitors MLCC - SMD/SMT"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_FerriteBead",
+        extends_symbol="Device.kicad_symdir/FerriteBead.kicad_sym",
+        package_source=ferritebead_packages,
+        name_template = '"_".join(filter(None,[data.get("Impedance @ Frequency"),package_name,data.get("Current Rating", ""),data.get("Tolerance", "")]))',
+        value_template="data['Impedance @ Frequency']",
+        category_filter='"Subcategory" LIKE "Ferrite Beads"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_LED",
+        extends_symbol="Device.kicad_symdir/LED.kicad_sym",
+        package_source=led_packages,
+        name_template = '"_".join(filter(None,[data.get("Illumination Color", "").replace(" ", "_"),package_name,data.get("Forward Current", ""),data.get("Luminous Intensity", "")]))',
+        value_template="data['Illumination Color']",
+        category_filter='category="Optoelectronics" and "Subcategory"="LED Indication - Discrete"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Diode-Zener",
+        extends_symbol="Device.kicad_symdir/D_Zener.kicad_sym",
+        package_source=diode_packages,
+        name_template="mfg_part",
+        value_template="mfg_part",
+        category_filter='category="Diodes" and "Subcategory"="Zener Diodes"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Diode-Schottky",
+        extends_symbol="Device.kicad_symdir/D_Schottky.kicad_sym",
+        package_source=diode_packages,
+        name_template="mfg_part",
+        value_template="mfg_part",
+        category_filter='category="Diodes" and "Subcategory"="Schottky Diodes"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Mosfet_NPN",
+        extends_symbol_lookup={
+            "2N7002*": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "AO3400*": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "HL2303": "Transistor_FET.kicad_symdir/Q_NMOS_DGS.kicad_sym",
+            "BSS1*": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "HL2*": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "HL3*": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "FDV301N": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "2SK3018": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+            "MDD2302": "Transistor_FET.kicad_symdir/Q_NMOS_GSD.kicad_sym",
+        },
+        package_source=transistor_packages,
+        name_template="mfg_part",
+        value_template="mfg_part",
+        category_filter='LOWER(category)="transistors/thyristors" and LOWER(Subcategory)="mosfets" and LOWER(json_extract(attributes, "$.Number")) = "1 n-channel"',
+    ),
+    LibrarySpec(
+        libname="JLCPCB_Basic_Mosfet_PNP",
+        extends_symbol_lookup={
+            "LBSS84*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "AO3401*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "MDD*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "SI2301*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "BSS84": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "HL2*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "HL3*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+            "HL6*": "Transistor_FET.kicad_symdir/Q_PMOS_GSD.kicad_sym",
+        },
+        package_source=transistor_packages,
+        name_template="mfg_part",
+        value_template="mfg_part",
+        category_filter='LOWER(category)="transistors/thyristors" and LOWER(Subcategory)="mosfets" and LOWER(json_extract(attributes, "$.Number")) = "1 p-channel"',
+    ),
+]
